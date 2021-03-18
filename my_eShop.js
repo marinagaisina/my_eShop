18f3464d0af7f6628399191e99b8c54cf29e6d45
Marina Gaisina <marinawhite3429@gmail.com>
2021-03-18 15:04:56 -0400

    Code corrections: - destructuring parameters in renderGoodsItem function; -onWindowLoad function added; -default parameter corrected (empty array).

--- my_eShop.js
+++ my_eShop.js
@@ -1,17 +1,26 @@
-// const goods = [
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-// ];
-//
-// const renderGoodsItem = (title, price) => {
-//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-// };
-//
-// const renderGoodsList = (list) => {
-//     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-//     document.querySelector(".goods-list").innerHTML = goodsList;
-// }
-//
-// renderGoodsList(goods);
+'use strict';
+const onWindowLoad = () => {
+    const goods = [
+        {title: 'Shirt', img: 'img/shirt.jpg', price: 150},
+        {title: 'Socks', img: 'img/socks.jpg', price: 50},
+        {title: 'Jacket', img: 'img/jacket.jpg', price: 350},
+        {title: 'Shoes', img: 'img/shoes.jpg', price: 250},
+        {title: 'Socks1', img: 'img/socks.jpg', price: 50},
+        {title: 'Shirt1', img: 'img/shirt.jpg', price: 150},
+    ];
+
+    const renderGoodsItem = ({ img, title, price }) =>
+        `<div class="goods-item d-flex flex-column rounded-lg bg-light mr-2 ml-2 mb-4 border">
+                <img class="img-fluid mb-auto" src="${img}" alt="img">
+                <div class="d-flex justify-content-between">
+                    <h3>${title}</h3><p>$${price}</p>
+                    <button class="btn btn-warning">Add to cart</button>
+                </div>
+         </div>`;
+
+    const renderGoodsList = (list = []) =>
+        document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item)).join("");
+
+    renderGoodsList(goods);  //вызов функции с параметром по умолчанию
+}
+window.onload = onWindowLoad;
