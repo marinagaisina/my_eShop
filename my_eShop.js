f70a8fdb6cd7362446f8958e8a60a3fe3748bae9
Marina Gaisina <marinawhite3429@gmail.com>
2021-03-19 15:43:08 -0400

    - default param corrected in renderGoodsList; - window.onload corrected; - testPage added.

--- my_eShop.js
+++ my_eShop.js
@@ -1,29 +1,31 @@
-// 'use strict';
-// const goods = [
-//     {title: 'Shirt', img: 'img/shirt.jpg', price: 150},
-//     {title: 'Socks', img: 'img/socks.jpg', price: 50},
-//     {title: 'Jacket', img: 'img/jacket.jpg', price: 350},
-//     {title: 'Shoes', img: 'img/shoes.jpg', price: 250},
-//     {title: 'Socks1', img: 'img/socks.jpg', price: 50},
-//     {title: 'Shirt1', img: 'img/shirt.jpg', price: 150},
-// ]
-//
-// const DEFAULT_ITEM =
-//     {title: 'Title', img: 'img/blank.jpg', price: 0}
-//
-// const renderGoodsItem = ({ img, title, price } = DEFAULT_ITEM) =>
-//     `<div class="goods-item d-flex flex-column rounded-lg bg-light mr-2 ml-2 mb-4 border">
-//             <img class="img-fluid mb-auto" src="${img}" alt="img">
-//             <div class="d-flex justify-content-between">
-//                 <h3>${title}</h3><p>$${price}</p>
-//                 <button class="btn btn-warning">Add to cart</button>
-//             </div>
-//      </div>`;
-//
-// const renderGoodsList = (list = []) =>
-//     document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item)).join('');
-//
-// //renderGoodsList(goods);
-//
-//
-// window.onload = () => renderGoodsList();  //вызов функции с параметром по умолчанию, пустой массив => renderGoodsItem(object DEFAULT_ITEM)
+'use strict';
+const goods = [
+    {title: 'Shirt', img: 'img/shirt.jpg', price: 150},
+    {title: 'Socks', img: 'img/socks.jpg', price: 50},
+    {title: 'Jacket', img: 'img/jacket.jpg', price: 350},
+    {title: 'Shoes', img: 'img/shoes.jpg', price: 250},
+    {title: 'Socks1', img: 'img/socks.jpg', price: 50},
+    {title: 'Shirt1', img: 'img/shirt.jpg', price: 150},
+]
+
+const DEFAULT_ITEM =
+    {title: 'defaultTitle', img: 'img/blank.jpg', price: 0}
+
+const renderGoodsItem = ({ img, title, price } = DEFAULT_ITEM) =>
+    `<div class="goods-item d-flex flex-column rounded-lg bg-light mr-2 ml-2 mb-4 border">
+            <img class="img-fluid mb-auto" src="${img}" alt="img">
+            <div class="d-flex justify-content-between">
+                <h3>${title}</h3><p>$${price}</p>
+                <button class="btn btn-warning">Add to cart</button>
+            </div>
+     </div>`;
+
+const renderGoodsList = (list =  [DEFAULT_ITEM]) =>
+    document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item)).join('');
+
+window.onload = () => {
+    renderGoodsList(goods);
+//    renderGoodsList();  // формирование списка товаров со значением по умолчанию, массив с одним элементом [DEFAULT_ITEM]
+    const $el = document.querySelector('.products-list');
+    $el.innerHTML = `<ul>${renderProductsList(products)}</ul>`
+}
--- test.js
+++ test.js
@@ -1,36 +1,35 @@
-// const products = [
-//     {
-//         name: 'Product#1',
-//         price: 100,
-//     },
-//     {
-//         name: 'Product#2',
-//         price: 98,
-//     },
-//     {
-//         name: 'Product#3',
-//         price: 23,
-//     },
-//     {
-//         name: 'Product#4',
-//         price: 987,
-//     }
-// ]
+const products = [
+    {
+        name: 'Product#1',
+        price: 100,
+    },
+    {
+        name: 'Product#2',
+        price: 98,
+    },
+    {
+        name: 'Product#3',
+        price: 23,
+    },
+    {
+        name: 'Product#4',
+        price: 987,
+    }
+]
 
-const DEFAULT_PRODUCT = {
-    name: 'Default',
-    price: 999
-}
+const DEFAULT_LIST = [
+    { name: 'Default', price: 999 }
+]
 
-const renderProduct = ({ name, price } = DEFAULT_PRODUCT) => {
+const renderProduct = ({ name, price } = DEFAULT_LIST[0]) => {
     return `<li>${name}: $${price}</li>`
 }
 
-const renderProductsList = (products = []) => {
+const renderProductsList = (products = DEFAULT_LIST) => {
     return products.map(p => renderProduct(p)).join('');
 }
 
-window.onload = () => {
-    const $el = document.querySelector('.goods-list');
-    $el.innerHTML = `<ul>${renderProductsList()}</ul>`
-};
+// window.onload = () => {
+//     const $el = document.querySelector('.products-list');
+//     $el.innerHTML = `<ul>${renderProductsList(products)}</ul>`
+// };
