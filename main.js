76562b2bd125fa44282f08b416d0ee6390cb5ce6
Marina Gaisina <marinawhite3429@gmail.com>
2021-03-30 21:30:52 -0400

    Feature Promise was added and it works

---
+++ main.js
@@ -0,0 +1,31 @@
+'use strict';
+const url1 = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
+
+const DEFAULT_PRODUCT = new GoodsItem(0, 'Default Item', 0);
+const list = new GoodsList();
+const cart = new CartList();
+
+window.onload = () => {
+    list.fetchGoods(url1);
+    cart.render();
+}
+
+
+
+
+
+
+// if (document.addEventListener) {
+//     document.addEventListener("click", handleClick, false);
+// }
+// else if (document.attachEvent) {
+//     document.attachEvent("onclick", handleClick);
+// }
+
+// document.getElementsByTagName('input').forEach(el => {
+//     el.addEventListener('onChange', cart.updateQuantity);
+// });
+// const inputs = document.getElementsByTagName('input');
+// for (let i=0;i<inputs.length;i++) {
+//     inputs[i].addEventListener('number', cart.updateQuantity);
+// }
