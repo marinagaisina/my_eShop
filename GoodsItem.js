14551d18ceac84e2901bc2cb767f9a91d433df60
marinagaisina <60253949+marinagaisina@users.noreply.github.com>
2021-04-04 10:59:15 -0400

    Features of getting data from server via XMLHttpRequest and Promise implemented. (#3)
    * makeGETRequest function implemented
    
    * makeGETRequest updated (in case a resource URL isn't found, an array with the default product will be used)
    
    * Feature Promise was added and it works
    
    * Проработала замечания по Lesson2 pull-request:
    -added static hasItem() that returns an element in a list - that is passed as a parameter;
    -added filter function to removeItem() instead of using a for loop;
    -forEach was replaced by reduce function in calculateTotal();
    -CartItems.render() modified;
    
    Co-authored-by: Marina Gaisina <marinawhite3429@gmail.com>

--- GoodsItem.js
+++ GoodsItem.js
@@ -1,18 +1,18 @@
 'use strict';
 class GoodsItem {
-    constructor(id, title, img, price) {
-        this.id = id;
-        this.title = title;
-        this.img = img;
+    constructor(id, name, price) {
+        this.id_product = id;
+        this.product_name = name;
+        //this.img = img;
         this.price = price;
     }
     render() {
-        return `<div id="${this.id}" class="goods-item d-flex flex-column rounded-lg bg-light mr-2 ml-2 mb-4 border">
-                    <img class="img-fluid mb-auto" src="${this.img}" alt="img">
-                    <h3>${this.title}</h3>
+        return `<div id="${this.id_product}" class="goods-item d-flex flex-column rounded-lg bg-light mr-2 ml-2 mb-4 border">
+                    <!-- <img class="img-fluid mb-auto" src="" alt="img"> -->
+                    <h3>${this.product_name}</h3>
                     <div class="d-flex justify-content-between">
-                        <p>$${this.price}</p>
-                        <button class="addToCart btn btn-warning" onclick="cart.addToCart(${this.id})">Add to cart</button>
+                        <p>${this.price}</p>
+                        <button class="addToCart btn btn-warning" onclick="cart.addToCart(${this.id_product})">Add to cart</button>
                     </div>
                 </div>`;
     }
